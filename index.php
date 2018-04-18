54362220cedc620642ededbb80406b7223bbf46a
smartzoe <asuquooduduabasi@gmail.com>
2018-04-18 16:57:44 +0100

    my first commit on git

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+date_default_timezone_set ('Africa/Lagos')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body> 
+    <h1 class="header">ODUDU-ABASI ASUQUO</h1>
+    <h2 class="h2">Today is <?php echo date ("l")?>.</h2>
+    <h3 class="line3">The Time is <?php echo date ("h:ia") ?></h3>
+
+    <p class="p">Join HNG Internship and learn how to code from scratch and get paid while at it.</p>
+
+    <div class="content">It's Simple!<br>
+                        It's Easy!<br>
+                        It Pays!<br><br>
+
+                        Join Now!!!</div>
+   
+</body>
+</html>
